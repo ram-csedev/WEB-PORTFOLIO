1e813069f5c11f17850a23d1fa1b99467351a1ac
ram <ram.csedev@gmail.com>
2025-06-17 20:18:18 +0530

    portfolio and new columns final

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
@@ -14,3 +16,18 @@ content2='''
 Below are some of the apps that i have created with the help of python.Feel free to contact me teehee
 '''
 st.write(content2)
+col3,col4 = st.columns(2)
+
+df=pandas.read_csv('data.csv',sep=';')
+with col3:
+    for index,row in df[0:10].iterrows():
+        st.header(row['title'])
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row['title'])
+
+
+
+
+
+
