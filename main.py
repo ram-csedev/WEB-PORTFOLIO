aa20b45a17867001e775fa975c94bba6ff767b66
ram <ram.csedev@gmail.com>
2025-06-17 20:29:37 +0530

    portfolio and new columns final

--- main.py
+++ main.py
@@ -16,17 +16,21 @@ content2='''
 Below are some of the apps that i have created with the help of python.Feel free to contact me teehee
 '''
 st.write(content2)
-col3,col4 = st.columns(2)
+col3,empycol,col4 = st.columns([1.5,0.5,1.5])
 
 df=pandas.read_csv('data.csv',sep=';')
 with col3:
     for index,row in df[0:10].iterrows():
         st.header(row['title'])
+        st.image('image/'+row['image'])
+        st.write(row['description'])
+        st.write(f"[Source Code]({row['url']})")
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row['title'])
-
-
+        st.image('image/' + row['image'])
+        st.write(row['description'])
+        st.write(f"[Source Code]({row['url']})")
 
 
 
